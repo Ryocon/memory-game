dd3515d31951ef36b9b0b4a7d5474262e5514c9a
roconn <roconn25@gmail.com>
2023-06-01 12:13:29 +0100

    header component + scoring logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,24 @@
 import './App.css'
 import { useState } from 'react'
+import Header from './components/Header'
 
 
 
 function App() {
 
+const [score, setScore] = useState(0) 
+const [highScore, setHighScore] = useState(0)
+// const [reset, setReset] = useState(true)
 
   return (
     <>
     <div>
       hi
     </div>
+    <Header 
+    score={score}
+    highScore={highScore}
+    />
     </>
   )
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,58 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+
+const Container = styled.div`
+    
+`
+
+const Left = styled.div`
+    
+`
+
+const Right = styled.div`
+    
+`
+
+const Title = styled.h1`
+    
+`
+
+const Score = styled.div`
+    
+`
+const HighScore = styled.div`
+    
+`
+
+const Description = styled.div`
+    
+`
+
+const Header = (props) => {
+    return (
+        <Container>
+            <Left>
+                <Title>Memory Game!</Title>
+                <Description>Match the images or something</Description>
+            </Left>
+            <Right>
+                <Score>
+                Score: {props.score}
+                </Score>
+                <HighScore>
+                   High Score: {props.highScore}
+                </HighScore>
+            </Right>
+            
+        </Container>
+    )
+}
+
+// To check if the prop is the correct type of data
+// Header.propTypes = {
+//     score: PropTypes.number,
+//     highScore: PropTypes.number,
+// }
+
+export default Header
